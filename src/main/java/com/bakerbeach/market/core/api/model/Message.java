74f12213a5098ec5be99eafd203dde87bf4e5dea
achimwolf <achim.wolf@baker-beach.com>
2017-03-06 19:03:14 +0100

    warning message type

--- src/main/java/com/bakerbeach/market/core/api/model/Message.java
+++ src/main/java/com/bakerbeach/market/core/api/model/Message.java
@@ -6,6 +6,7 @@ public interface Message {
 	
 	public final static String TYPE_INFO = "info";
 	public final static String TYPE_ERROR = "error";
+	public final static String TYPE_WARNING = "error";
 	public final static String TYPE_COMMAND = "command";
 
 	String getType();
